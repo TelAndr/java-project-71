f757f7f34a62ce01919f0e152792945c37eea7c9
TelAndr <atelikh@gmail.com>
2025-09-10 01:01:25 +0300

    modified method format() in JsonFormatters.java

--- app/src/main/java/hexlet/code/formatters/JsonFormatter.java
+++ app/src/main/java/hexlet/code/formatters/JsonFormatter.java
@@ -14,41 +14,8 @@ public class JsonFormatter implements Format {
     @Override
     //public String format(Map<String, Object> mapBefore, Map<String, Object> mapAfter) {
     public String format(Map<String, Status > resultDiffMap) {
-        Map<String, Object> differences = new HashMap<>();
-        //Set<String> allKeys = new TreeSet<>(mapBefore.keySet());
-        Set<String> allDiffKeys = new TreeSet<>(resultDiffMap.keySet());
-        //allKeys.addAll(mapBefore.keySet());
-        //allKeys.addAll(mapAfter.keySet());
-
-        //for (String key : allKeys) {
-        for (String key : allDiffKeys) {
-            //Object objValBefore = mapBefore.get(key);
-            //Object objValAfter = mapAfter.get(key);
-            Status objStatusVal = resultDiffMap.get(key);
-
-            switch(objStatusVal.getStatusName()) {
-                case "deleted":
-                    differences.put(key, Map.of("status", "added", "newValue", objStatusVal.getOldValue()));
-                    break;
-                case "changed":
-                    differences.put(key, Map.of("status", "changed", "oldValue", objStatusVal.getOldValue(),
-                            "newValue", objStatusVal.getNewValue()));
-                    break;
-                case "added":
-                    differences.put(key, Map.of("status", "removed", "oldValue", objStatusVal.getNewValue()));
-                    break;
-            }
-
-            //if (!mapBefore.containsKey(key)) {
-            //    differences.put(key, Map.of("status", "added", "newValue", objValAfter));
-            //} else if (!mapAfter.containsKey(key)) {
-            //    differences.put(key, Map.of("status", "removed", "oldValue", objValBefore));
-            //} else if (!objValBefore.equals(objValAfter)) {
-            //    differences.put(key, Map.of("status", "changed", "oldValue", objValBefore,
-            //            "newValue", objValAfter));
-            //}
-        }
-        return convertToJson(differences);
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(resultDiffMap);
     }
     private String convertToJson(Map<String, Object> data) {
         ObjectMapper objMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
