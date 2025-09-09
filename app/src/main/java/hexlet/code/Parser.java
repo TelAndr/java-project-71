c1f8349fc7c48efcef6a1fe44371237931d5e3f2
TelAndr <atelikh@gmail.com>
2025-09-10 00:58:54 +0300

    modified method parseJsonOrYamlFile() in Parser.java

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -3,12 +3,26 @@ package hexlet.code;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
 public class Parser {
     public Map<String, Object> parseYaml(String filePath) throws Exception {
         Yaml yaml = new Yaml();
         try (InputStream inputStream = YamlDiff.class.getClassLoader().getResourceAsStream(filePath)) {
             return yaml.load(inputStream);
         }
+    }
+    public static Map<String, Object> parseJsonOrYamlFile(String strFileType, String filePath) throws Exception {
+        Map<String, Object> fileData = new HashMap<>();
+        switch (strFileType) {
+            case "json":
+                fileData = JsonDiff.parseJson(filePath);
+                break;
+            case "yaml":
+                fileData = Differ.loadYaml(filePath);
+        }
+        return fileData;
     }
 }
