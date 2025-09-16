b8f3eb8ada8661480b3116175e18bfbc41cefd7e
TelAndr <atelikh@gmail.com>
2025-09-16 23:14:07 +0300

    add method parse()

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.InputStream;
@@ -8,12 +10,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Parser {
-    public Map<String, Object> parseYaml(String filePath) throws Exception {
-        Yaml yaml = new Yaml();
-        try (InputStream inputStream = YamlDiff.class.getClassLoader().getResourceAsStream(filePath)) {
-            return yaml.load(inputStream);
-        }
-    }
+    //public Map<String, Object> parseYaml(String filePath) throws Exception {
+    //    Yaml yaml = new Yaml();
+    //    try (InputStream inputStream = YamlDiff.class.getClassLoader().getResourceAsStream(filePath)) {
+    //        return yaml.load(inputStream);
+    //    }
+    //}
     public static Map<String, Object> parseJsonOrYamlFile(String strFileType, String filePath) throws Exception {
         Map<String, Object> fileData = new HashMap<>();
         switch (strFileType) {
@@ -24,5 +26,27 @@ public class Parser {
                 fileData = Differ.loadYaml(filePath);
         }
         return fileData;
+    }
+
+    private static Map parseYaml(String content) throws Exception  {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        return mapper.readValue(content, Map.class);
+    }
+
+    private static Map parseJson(String content) throws Exception  {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(content, Map.class);
+    }
+
+    public static Map parse(String content, String dataFormat) throws Exception {
+        switch (dataFormat) {
+            case "yml":
+            case "yaml":
+                return parseYaml(content);
+            case "json":
+                return parseJson(content);
+            default:
+                throw new Exception("Unknown format: '" + dataFormat + "'");
+        }
     }
 }
