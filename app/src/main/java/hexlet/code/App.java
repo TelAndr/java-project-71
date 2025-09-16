3217b4d4d0e5c9f0dfbdb62f3370017c6fd49d03
TelAndr <atelikh@gmail.com>
2025-09-16 23:07:13 +0300

    patches command line

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -21,15 +21,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 //public class App {
+@CommandLine.Command(name = "app", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference.")
 public final class App implements Callable<Integer> {
     //filePath1, filePath2 ...
     //@Option
     //@Parameters
-    Path filePath1 = Path.of("src/main/resources/file1.json");
-    Path filePath2 = Path.of("src/main/resources/file2.json");
+    @CommandLine.Parameters(index = "0", description = "The path to the first file.")
+    private Path filePath1; // = Path.of("src/main/resources/file1.json");
+    @CommandLine.Parameters(index = "1", description = "The path to the second file.")
+    private Path filePath2; //= Path.of("src/main/resources/file2.json");
     String strFilePath1 = filePath1.toString();
     String strFilePath2 = filePath2.toString();
-    String formatName = "plain";
+    @CommandLine.Option(names = {"-f", "--format"}, description = "The format output (default: stylish).",
+            defaultValue = "stylish")
+    private String formatName = "plain";
 
     @Override
     public Integer call() {
@@ -66,8 +72,10 @@ public final class App implements Callable<Integer> {
     //    return instance;
     //}
     public static void main(String[] args) throws Exception {
-            System.out.printf("Hello World!");
-            Path pathFile1 = Paths.get("file1.json");
-            Path pathFile2 = Paths.get("file2.json");
+        System.out.printf("Hello World!");
+        Path pathFile1 = Paths.get("file1.json");
+        Path pathFile2 = Paths.get("file2.json");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
