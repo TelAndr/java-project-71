4d09d2478008f904854cd57c30ae706bae14ee7b
TelAndr <atelikh@gmail.com>
2025-09-23 21:02:01 +0300

    modified Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -4,6 +4,13 @@ import java.util.Map;
 import hexlet.code.formatters.StylishFormatter;
 import hexlet.code.formatters.PlainFormatter;
 public class Formatter {
+    /**
+     * Форматирует различия между двумя структурами данных.
+     * Этот метод может быть переопределен в подклассах для изменения поведения.
+     *
+     * @param resultDiffMap карта различий, которую нужно отформатировать.
+     * @return отформатированная строка.
+     */
     public String format(Map<String, Status> resultDiffMap) {
         return "default string";
     }
