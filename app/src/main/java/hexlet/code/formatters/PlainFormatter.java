b5988a51f360df2052ecc34d4b55101dc7fced09
TelAndr <atelikh@gmail.com>
2025-09-23 21:12:58 +0300

    modified PlainFormatter.java

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -1,62 +1,45 @@
 package hexlet.code.formatters;
-import hexlet.code.Formatter;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import hexlet.code.Formatter.Format;
 import hexlet.code.Status;
 
 public class PlainFormatter implements Format {
+    /**
+     * Форматирует различия между двумя структурами данных формата Plain.
+     *
+     * @param resultDiffMap карта различий, которую нужно отформатировать.
+     * @return отформатированная строка.
+     */
     @Override
-    //public String format(Map<String, Object> mapBefore, Map<String, Object> mapAfter) {
     public String format(Map<String, Status> resultDiffMap) {
         StringBuilder resStrBuilder = new StringBuilder();
-        //Map<String, Status> resultDiffMap = new HashMap<>();
-
-        Set<String> allKeys = new TreeSet<>();
-        //allKeys.addAll(mapBefore.keySet());
-        //allKeys.addAll(mapAfter.keySet());
-
         Set<String> allDiffKeys = new TreeSet<>();
         allDiffKeys.addAll(resultDiffMap.keySet());
 
-        //for (String key : allKeys) {
         for (String key : allDiffKeys) {
-                //Object objValBefore = mapBefore.get(key);
-                //Object objValAfter = mapAfter.get(key);
-                Status objStatusVal = resultDiffMap.get(key);
+            Status objStatusVal = resultDiffMap.get(key);
 
-                switch(objStatusVal.getStatusName()) {
-                    case "deleted":
-                        //resStrBuilder.append("  - ").append(key).append(": ").append(objStatusVal.getOldValue()).append("\n");
-                        resStrBuilder.append("Property '").append(key).append("' was removed\n");
-                        break;
-                    case "changed":
-                        //resStrBuilder.append("  - ").append(key).append(": ").append(objStatusVal.getOldValue()).append("\n");
-                        //resStrBuilder.append("  + ").append(key).append(": ").append(objStatusVal.getNewValue()).append("\n");
-                        resStrBuilder.append("Property '").append(key).append("' was updated. From ")
-                                .append(printValue(objStatusVal.getOldValue())).append(" to ")
-                                .append(printValue(objStatusVal.getNewValue())).append("\n");
-                        break;
-                    case "added":
-                        //resStrBuilder.append("  + ").append(key).append(": ").append(objStatusVal.getNewValue()).append("\n");
-                        resStrBuilder.append("Property '").append(key).append("' was added with value: ")
-                                .append(printValue(objStatusVal.getNewValue())).append("\n");
-                        break;
-                }
-
-                //if (!mapBefore.containsKey(key)) {
-                //    resStrBuilder.append("Property '").append(key).append("' was added with value: ")
-                //            .append(printValue(objValAfter)).append("\n");
-                //} else if (!mapAfter.containsKey(key)) {
-                //    resStrBuilder.append("Property '").append(key).append("' was removed\n");
-                //} else if (!objValBefore.equals(objValAfter)) {
-                //    resStrBuilder.append("Property '").append(key).append("' was updated. From ")
-                //            .append(printValue(objValBefore)).append(" to ")
-                //            .append(printValue(objValAfter)).append("\n");
-                //}
+            switch (objStatusVal.getStatusName()) {
+                case "deleted":
+                    resStrBuilder.append("Property '").append(key).append("' was removed\n");
+                    break;
+                case "changed":
+                    resStrBuilder.append("Property '").append(key).append("' was updated. From ")
+                            .append(printValue(objStatusVal.getOldValue())).append(" to ")
+                            .append(printValue(objStatusVal.getNewValue())).append("\n");
+                    break;
+                case "added":
+                    resStrBuilder.append("Property '").append(key).append("' was added with value: ")
+                            .append(printValue(objStatusVal.getNewValue())).append("\n");
+                    break;
+                default:
+                    resStrBuilder.append("Not set status value. Can not create Property!");
             }
-
+        }
         return resStrBuilder.toString().trim();
     }
     private static String printValue(Object objVal) {
@@ -70,25 +53,4 @@ public class PlainFormatter implements Format {
             return objVal.toString();
         }
     }
-    //public static void main(String[] args) {
-    //    Map<String, Object> mapBefore = Map.of(
-    //            "chars1", List.of("a", "b", "c"),
-    //            "chars2", List.of("d", "e", "f"),
-    //            "checked", false,
-    //            "default", null,
-    //            "id", 45
-    //    );
-
-    //    Map<String, Object> mapAfter = Map.of(
-    //            "chars1", List.of("a", "b", "c"),
-    //            "chars2", false,
-    //            "checked", true,
-    //            "default", List.of("value1", "value2"),
-    //            "id", null,
-    //            "key1", "value1"
-    //    );
-
-    //    PlainFormatter plForm = new PlainFormatter();
-    //    System.out.println(plForm.format(mapBefore, mapAfter));
-    //}
 }
