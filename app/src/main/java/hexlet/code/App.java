deabc960f6be0f9d2c9886a615606a82f0d3e5aa
TelAndr <atelikh@gmail.com>
2025-09-23 21:00:12 +0300

    modified App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,24 +1,9 @@
 package hexlet.code;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
-
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.security.MessageDigest;
 import java.util.concurrent.Callable;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.HashMap;
-import java.util.Map;
 
 //public class App {
 @CommandLine.Command(name = "app", mixinStandardHelpOptions = true,
@@ -31,8 +16,8 @@ public final class App implements Callable<Integer> {
     private Path filePath1; // = Path.of("src/main/resources/file1.json");
     @CommandLine.Parameters(index = "1", description = "The path to the second file.")
     private Path filePath2; //= Path.of("src/main/resources/file2.json");
-    String strFilePath1 = filePath1.toString();
-    String strFilePath2 = filePath2.toString();
+    private String strFilePath1 = filePath1.toString();
+    private String strFilePath2 = filePath2.toString();
     @CommandLine.Option(names = {"-f", "--format"}, description = "The format output (default: stylish).",
             defaultValue = "stylish")
     private String formatName = "plain";
@@ -50,27 +35,6 @@ public final class App implements Callable<Integer> {
 
         return 0;
     }
-    //public static void saveProxy(Path filePath, DataHostProxy instance) throws Exception {
-    //    String jsonRepresentation = instance.serialize();
-    //    Files.writeString(filePath, jsonRepresentation, StandardOpenOption.WRITE);
-    //}
-
-    //public static DataHostProxy extractProxy(Path filePath) throws Exception {
-    //    String jsonRepresentation = Files.readString(filePath);
-    //    DataHostProxy instance = DataHostProxy.unserialize(jsonRepresentation);
-    //    return instance;
-    //}
-
-    //public static void saveVerbose(Path filePath, DataHostVerbose instance) throws Exception {
-    //    String jsonRepresentation = instance.serialize();
-    //    Files.writeString(filePath, jsonRepresentation, StandardOpenOption.WRITE);
-    //}
-
-    //public static DataHostVerbose extractVerbose(Path filePath) throws Exception {
-    //    String jsonRepresentation = Files.readString(filePath);
-    //    DataHostVerbose instance = DataHostVerbose.unserialize(jsonRepresentation);
-    //    return instance;
-    //}
     public static void main(String[] args) throws Exception {
         System.out.printf("Hello World!");
         Path pathFile1 = Paths.get("file1.json");
