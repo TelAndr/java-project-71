211c8c7ef2630e0eec408cf7ca5dbc6cfcfcfcea
TelAndr <atelikh@gmail.com>
2025-09-23 21:13:40 +0300

    modified StylishFormatter.java

--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -1,77 +1,47 @@
 package hexlet.code.formatters;
-import hexlet.code.Formatter;
 
-import java.util.*;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import hexlet.code.Formatter.Format;
 import hexlet.code.Status;
 
 public class StylishFormatter implements Format {
+    /**
+     * Форматирует различия между двумя структурами данных формата Stylish.
+     *
+     * @param resultDiffMap карта различий, которую нужно отформатировать.
+     * @return отформатированная строка.
+     */
     @Override
-    //public String format(Map<String, Object> mapBefore, Map<String, Object> mapAfter) {
     public String format(Map<String, Status> resultDiffMap) {
         StringBuilder resStrBuilder = new StringBuilder("{\n");
-        //Map<String, Status> resultDiffMap = new HashMap<>();
-
-        Set<String> allKeys = new TreeSet<>();
-        //allKeys.addAll(mapBefore.keySet());
-        //allKeys.addAll(mapAfter.keySet());
-
         Set<String> allDiffKeys = new TreeSet<>();
         allDiffKeys.addAll(resultDiffMap.keySet());
 
-        //for (String key : allKeys) {
         for (String key : allDiffKeys) {
-            //Object valueBefore = mapBefore.get(key);
-            //Object valueAfter = mapAfter.get(key);
             Status objStatusVal = resultDiffMap.get(key);
 
-            switch(objStatusVal.getStatusName()) {
+            switch (objStatusVal.getStatusName()) {
                 case "deleted":
-                    resStrBuilder.append("  - ").append(key).append(": ").append(objStatusVal.getOldValue()).append("\n");
+                    resStrBuilder.append("  - ").append(key).append(": ")
+                            .append(objStatusVal.getOldValue()).append("\n");
                     break;
                 case "changed":
-                    resStrBuilder.append("  - ").append(key).append(": ").append(objStatusVal.getOldValue()).append("\n");
-                    resStrBuilder.append("  + ").append(key).append(": ").append(objStatusVal.getNewValue()).append("\n");
+                    resStrBuilder.append("  - ").append(key).append(": ")
+                            .append(objStatusVal.getOldValue()).append("\n");
+                    resStrBuilder.append("  + ").append(key).append(": ")
+                            .append(objStatusVal.getNewValue()).append("\n");
                     break;
                 case "added":
-                    resStrBuilder.append("  + ").append(key).append(": ").append(objStatusVal.getNewValue()).append("\n");
+                    resStrBuilder.append("  + ").append(key).append(": ")
+                            .append(objStatusVal.getNewValue()).append("\n");
+                default:
+                    resStrBuilder.append("Not set status value. Can not create Property!");
             }
-            //if (!mapBefore.containsKey(key)) {
-                // Key added
-            //    resStrBuilder.append("  + ").append(key).append(": ").append(valueAfter).append("\n");
-            //} else if (!mapAfter.containsKey(key)) {
-                // Key removed
-            //    resStrBuilder.append("  - ").append(key).append(": ").append(valueBefore).append("\n");
-            //} else if (!valueBefore.equals(valueAfter)) {
-                // Key changed
-            //    resStrBuilder.append("  - ").append(key).append(": ").append(valueBefore).append("\n");
-            //    resStrBuilder.append("  + ").append(key).append(": ").append(valueAfter).append("\n");
-            //} else {
-                // Key unchanged
-            //    resStrBuilder.append("    ").append(key).append(": ").append(valueBefore).append("\n");
-            //}
         }
         resStrBuilder.append("}");
-
         return resStrBuilder.toString();
     }
-    //public static void main(String[] args) {
-    //    Map<String, Object> before = Map.of(
-    //            "chars1", List.of("a", "b", "c"),
-    //            "chars2", List.of("d", "e", "f"),
-    //            "checked", false,
-    //            "default", null
-    //    );
-
-    //    Map<String, Object> after = Map.of(
-    //            "chars1", List.of("a", "b", "c"),
-    //            "chars2", false,
-    //            "checked", true,
-    //            "default", List.of("value1", "value2")
-    //    );
-
-    //    StylishFormatter stForm = new StylishFormatter();
-    //    System.out.println(stForm.format(before, after));
-    //}
 }
