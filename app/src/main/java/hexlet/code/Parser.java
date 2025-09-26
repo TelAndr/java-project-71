1926d8dddc484f9213effbdd841f74540257556e
TelAndr <atelikh@gmail.com>
2025-09-26 21:30:09 +0300

    add ALLOW_COMMENTS

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -2,16 +2,20 @@ package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.core.JsonParser;
 import java.util.Map;
 
 public class Parser {
     private static Map parseYaml(String content) throws Exception  {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        ObjectMapper mapper =  new ObjectMapper(new YAMLFactory());
+        // JsonParserWithComments.createObjectMapper(new YAMLFactory());
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         return mapper.readValue(content, Map.class);
     }
 
     private static Map parseJson(String content) throws Exception  {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         return mapper.readValue(content, Map.class);
     }
 
