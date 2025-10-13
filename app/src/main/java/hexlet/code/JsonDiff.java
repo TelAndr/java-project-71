a56ac4f3f5de23723af946b64b852d88a500913a
TelAndr <atelikh@gmail.com>
2025-10-13 21:21:25 +0300

    change to one cicle in JsonDiff.java

--- app/src/main/java/hexlet/code/JsonDiff.java
+++ app/src/main/java/hexlet/code/JsonDiff.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public class JsonDiff {
     public static Map<String, Object> parseJson(String filePath) throws IOException {
@@ -16,6 +13,29 @@ public class JsonDiff {
         return json;
     }
     public static Map<String, Status> findDifferentsMap(Map<String, Object> mapJson1, Map<String, Object> mapJson2) {
+        Set<String> keys = new TreeSet (Comparator.naturalOrder());
+        keys.addAll(mapJson1.keySet());
+        keys.addAll(mapJson2.keySet());
+        Map<String, Status> diffMapStatus = new HashMap<>();
+        for (String curKeyMap : keys) {
+            if (mapJson1.containsKey(curKeyMap) && !mapJson2.containsKey(curKeyMap)) {
+                Status objStatus = new Status(Status.DELETED, mapJson1.get(curKeyMap), "");
+                diffMapStatus.put(curKeyMap, objStatus);
+            } else if (mapJson1.containsKey(curKeyMap) && !Objects.equals(mapJson1.get(curKeyMap), mapJson2.get(curKeyMap))) {
+                Status objStatus = new Status(Status.CHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
+                diffMapStatus.put(curKeyMap, objStatus);
+            } else {
+                Status objStatus = new Status(Status.UNCHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
+                diffMapStatus.put(curKeyMap, objStatus);
+            }
+            if (!mapJson1.containsKey(curKeyMap) && mapJson2.containsKey(curKeyMap)) {
+                Status objStatus = new Status(Status.ADDED, "", mapJson2.get(curKeyMap));
+                diffMapStatus.put(curKeyMap, objStatus);
+            }
+        }
+        return diffMapStatus;
+    }
+    /*public static Map<String, Status> findDifferentsMap(Map<String, Object> mapJson1, Map<String, Object> mapJson2) {
         Map<String, Object> diffMap = new HashMap<>();
         Map<String, Status> diffMapStatus = new HashMap<>();
         Set<String> setAllKeysJson1 = mapJson1.keySet();
@@ -61,5 +81,5 @@ public class JsonDiff {
         //setAllKeysJson2.stream().filter(curKeyMap -> (!mapJson1.containsKey(curKeyMap))).
         //        map(curKeyMap -> diffMap.put(curKeyMap, mapJson2.get(curKeyMap)));
         return diffMapStatus;
-    }
+    } */
 }
