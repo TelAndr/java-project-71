1361d8c0315da8ad883cb603796890d11c046ac7
TelAndr <atelikh@gmail.com>
2025-11-10 22:01:44 +0300

    rename 2 arguments in findDifferentsMap

--- app/src/main/java/hexlet/code/MapDiff.java
+++ app/src/main/java/hexlet/code/MapDiff.java
@@ -6,80 +6,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-public class JsonDiff {
-    public static Map<String, Object> parseJson(String filePath) throws IOException {
-        ObjectMapper objMapper = new ObjectMapper();
-        Map<String, Object> json = objMapper.readValue(new File("filePath"), Map.class);
-        return json;
-    }
-    public static Map<String, Status> findDifferentsMap(Map<String, Object> mapJson1, Map<String, Object> mapJson2) {
+public class MapDiff {
+    public static Map<String, Status> findDifferentsMap(Map<String, Object> mapObj1, Map<String, Object> mapObj2) {
         Set<String> keys = new TreeSet (Comparator.naturalOrder());
-        keys.addAll(mapJson1.keySet());
-        keys.addAll(mapJson2.keySet());
+        keys.addAll(mapObj1.keySet());
+        keys.addAll(mapObj2.keySet());
         Map<String, Status> diffMapStatus = new HashMap<>();
         for (String curKeyMap : keys) {
-            if (mapJson1.containsKey(curKeyMap) && !mapJson2.containsKey(curKeyMap)) {
-                Status objStatus = new Status(Status.DELETED, mapJson1.get(curKeyMap), "");
+            if (mapObj1.containsKey(curKeyMap) && !mapObj2.containsKey(curKeyMap)) {
+                Status objStatus = new Status(Status.DELETED, mapObj1.get(curKeyMap), "");
                 diffMapStatus.put(curKeyMap, objStatus);
-            } else if (mapJson1.containsKey(curKeyMap) && !Objects.equals(mapJson1.get(curKeyMap), mapJson2.get(curKeyMap))) {
-                Status objStatus = new Status(Status.CHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
+            } else if (mapObj1.containsKey(curKeyMap) && !Objects.equals(mapObj1.get(curKeyMap), mapObj2.get(curKeyMap))) {
+                Status objStatus = new Status(Status.CHANGED, mapObj1.get(curKeyMap), mapObj2.get(curKeyMap));
                 diffMapStatus.put(curKeyMap, objStatus);
             } else {
-                Status objStatus = new Status(Status.UNCHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
+                Status objStatus = new Status(Status.UNCHANGED, mapObj1.get(curKeyMap), mapObj2.get(curKeyMap));
                 diffMapStatus.put(curKeyMap, objStatus);
             }
-            if (!mapJson1.containsKey(curKeyMap) && mapJson2.containsKey(curKeyMap)) {
-                Status objStatus = new Status(Status.ADDED, "", mapJson2.get(curKeyMap));
+            if (!mapObj1.containsKey(curKeyMap) && mapObj2.containsKey(curKeyMap)) {
+                Status objStatus = new Status(Status.ADDED, "", mapObj2.get(curKeyMap));
                 diffMapStatus.put(curKeyMap, objStatus);
             }
         }
         return diffMapStatus;
     }
-    /*public static Map<String, Status> findDifferentsMap(Map<String, Object> mapJson1, Map<String, Object> mapJson2) {
-        Map<String, Object> diffMap = new HashMap<>();
-        Map<String, Status> diffMapStatus = new HashMap<>();
-        Set<String> setAllKeysJson1 = mapJson1.keySet();
-        Set<String> setAllKeysJson2 = mapJson2.keySet();
-        //Set<String> filteredKeysJson1;
-        //Set<String> filteredKeysJson2;
-
-        for (String curKeyMap : mapJson1.keySet()) {
-            //if (!mapJson2.containsKey(curKeyMap) || !mapJson1.get(curKeyMap).equals(mapJson2.get(curKeyMap))) {
-            if (!mapJson2.containsKey(curKeyMap) || !Objects.equals(mapJson1.get(curKeyMap), mapJson2.get(curKeyMap))) {
-                diffMap.put(curKeyMap, mapJson1.get(curKeyMap));
-            }
-        }
-
-        for (String curKeyMap : mapJson1.keySet()) {
-            if (!mapJson2.containsKey(curKeyMap)) {
-                Status objStatus = new Status(Status.DELETED, mapJson1.get(curKeyMap), "");
-                diffMapStatus.put(curKeyMap, objStatus);
-            } else if (!Objects.equals(mapJson1.get(curKeyMap), mapJson2.get(curKeyMap))) {
-                Status objStatus = new Status(Status.CHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
-                diffMapStatus.put(curKeyMap, objStatus);
-            } else {
-                Status objStatus = new Status(Status.UNCHANGED, mapJson1.get(curKeyMap), mapJson2.get(curKeyMap));
-                diffMapStatus.put(curKeyMap, objStatus);
-            }
-        }
-
-        //setAllKeysJson1.stream().filter(curKeyMap -> (!mapJson2.containsKey(curKeyMap) ||
-        // !mapJson1.get(curKeyMap).equals(mapJson2.get(curKeyMap))) ).
-        //        map(curKeyMap -> diffMap.put(curKeyMap, mapJson1.get(curKeyMap)));
-        for (String curKeyMap : mapJson2.keySet()) {
-            if (!mapJson1.containsKey(curKeyMap)) {
-                diffMap.put(curKeyMap, mapJson2.get(curKeyMap));
-            }
-        }
-
-        for (String curKeyMap : mapJson2.keySet()) {
-            if (!mapJson1.containsKey(curKeyMap)) {
-                Status objStatus = new Status(Status.ADDED, "", mapJson2.get(curKeyMap));
-                diffMapStatus.put(curKeyMap, objStatus);
-            }
-        }
-        //setAllKeysJson2.stream().filter(curKeyMap -> (!mapJson1.containsKey(curKeyMap))).
-        //        map(curKeyMap -> diffMap.put(curKeyMap, mapJson2.get(curKeyMap)));
-        return diffMapStatus;
-    } */
 }
