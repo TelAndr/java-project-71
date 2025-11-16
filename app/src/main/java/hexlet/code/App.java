2ee97f2c9e8e718cf299dda376007df3983e8888
TelAndr <atelikh@gmail.com>
2025-11-16 23:24:45 +0300

    edit strings with path1 and path2

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,24 +9,36 @@ import java.util.concurrent.Callable;
 @CommandLine.Command(name = "app", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public final class App implements Callable<Integer> {
-    //filePath1, filePath2 ...
-    //@Option
-    //@Parameters
-    @CommandLine.Parameters(index = "0", description = "The path to the first file.")
-    private Path filePath1; // = Path.of("src/main/resources/file1.json");
-    @CommandLine.Parameters(index = "1", description = "The path to the second file.")
-    private Path filePath2; //= Path.of("src/main/resources/file2.json");
-    private String strFilePath1 = filePath1.toString();
-    private String strFilePath2 = filePath2.toString();
-    @CommandLine.Option(names = {"-f", "--format"}, description = "The format output (default: stylish).",
-            defaultValue = "stylish")
-    private String formatName = "plain";
+    //@CommandLine.Parameters(index = "0", description = "The path to the first file.")
+    //private Path filePath1; // = Path.of("src/main/resources/file1.json");
+    //@CommandLine.Parameters(index = "1", description = "The path to the second file.")
+    //private Path filePath2; //= Path.of("src/main/resources/file2.json");
+    //private String strFilePath1 = filePath1.toString();
+    //private String strFilePath2 = filePath2.toString();
+    //@CommandLine.Option(names = {"-f", "--format"}, description = "The format output (default: stylish).",
+    //        defaultValue = "stylish")
+    //private String formatName = "plain";
+    @CommandLine.Option(names = {"-f", "--format"},
+            defaultValue = "stylish",
+            paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String formatName;
+
+    @CommandLine.Parameters(paramLabel = "filepath1",
+            index = "0",
+            description = "path to first file")
+    private String filePath1;
+
+    @CommandLine.Parameters(paramLabel = "filepath2",
+            index = "1",
+            description = "path to second file")
+    private String filePath2;
 
     @Override
     public Integer call() {
 
         try {
-            String formattedDiff = Differ.generate(strFilePath1, strFilePath2, formatName);
+            String formattedDiff = Differ.generate(filePath1, filePath2, formatName);
             System.out.println(formattedDiff);
         } catch (Exception e) {
             System.err.println(e.getMessage());
@@ -37,8 +49,8 @@ public final class App implements Callable<Integer> {
     }
     public static void main(String[] args) throws Exception {
         System.out.printf("Hello World!");
-        Path pathFile1 = Paths.get("file1.json");
-        Path pathFile2 = Paths.get("file2.json");
+        App objApp = new App();
+        int returnValCall = objApp.call();
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
