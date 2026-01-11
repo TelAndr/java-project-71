5a1dc95647c5e3b4401cf73aa96665c422e44b1d
TelAndr <atelikh@gmail.com>
2026-01-11 21:49:21 +0300

    fix problem with correct test

--- app/src/test/java/JsonIOExceptionTest.java
+++ app/src/test/java/JsonIOExceptionTest.java
@@ -1,4 +1,4 @@
-import com.fasterxml.jackson.databind.JsonMappingException;
+//import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
