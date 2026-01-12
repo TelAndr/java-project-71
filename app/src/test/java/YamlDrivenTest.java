cfaa704c06a2d6a1b62a9a267f5d76602b5123fb
TelAndr <atelikh@gmail.com>
2026-01-12 23:59:51 +0300

    fix correct rows in compare function

--- app/src/test/java/YamlDrivenTest.java
+++ app/src/test/java/YamlDrivenTest.java
@@ -1,15 +1,19 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
+//import hexlet.code.Parser;
 import hexlet.code.Status;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
+//import static hexlet.code.Differ;
 import static hexlet.code.MapDiff.findDifferentsMap;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class YamlDrivenTest {
@@ -28,19 +32,29 @@ public class YamlDrivenTest {
     @Test
     @DisplayName("Test case 1: Identical JSONs")
     public void testYamlDrivenScenario() throws Exception {
-        TestCase testCase = loadTestCase("test_cases/test_case_1.yml");
+        //TestCase testCase = loadTestCase("test_cases/test_case_1.yml");
 
         // Вызов тестируемой функции сравнения файлов
-        String strResult = compareJsonFiles(testCase.inputFile1, testCase.inputFile2);
-
+        String strJsonPath1 = "src/main/resources/file1.json";
+        String strJsonPath2 = "src/main/resources/file2.json";
+        String fileContent1 = Files.readString(Path.of(strJsonPath1));
+        String fileContent2 = Files.readString(Path.of(strJsonPath2));
+        String strFileType1 = "json";
+        String strFileType2 = "json";
+        String expectedStrVal = "not identical";
+        //String strResult = compareJsonFiles(testCase.inputFile1, testCase.inputFile2);
+        String strResult = compareJsonFiles(fileContent1, fileContent2);
         // Проверка результата
-        assertEquals(testCase.expectedResult, strResult, testCase.description);
+        assertEquals(expectedStrVal, strResult);
+        //assertEquals(testCase.expectedResult, strResult, testCase.description);
     }
     private String compareJsonFiles(String file1, String file2) throws IOException {
         // Здесь реализован ваш код для сравнения файлов
         ObjectMapper objMapper = new ObjectMapper();
-        Map<String, Object> json1 = objMapper.readValue(new File("file1"), Map.class);
-        Map<String, Object> json2 = objMapper.readValue(new File("file2"), Map.class);
+        //Map<String, Object> json1 = objMapper.readValue(new File("file1"), Map.class);
+        //Map<String, Object> json2 = objMapper.readValue(new File("file2"), Map.class);
+        Map<String, Object> json1 = objMapper.readValue(file1, Map.class);
+        Map<String, Object> json2 = objMapper.readValue(file2, Map.class);
         Map<String, Status> resultDiffMap = findDifferentsMap(json1, json2);
         return (resultDiffMap.isEmpty()) ? "identical" : "not identical";
     }
